c7b447ce1fe77cd872315efa32fece5d1b556036
RandomnameJ98 <60264904+RandomnameJ98@users.noreply.github.com>
2020-09-26 00:42:22 +0200

    Update game.js

--- src/main/resources/static/game.js
+++ src/main/resources/static/game.js
@@ -1,15 +1,10 @@
 var canvas = document.getElementById("main-canvas");
 var ctx = canvas.getContext("2d");
 
+var test = document.getElementById("test");
 
-ctx.fillStyle="#881212";
-ctx.strokeStyle="#121288";
-ctx.lineWidth=12;
+function drawimg(){
+	ctx.drawImage(test,0,0);
+}
 
-ctx.strokeRect(20,20,400,240);
-ctx.fillRect(20,20,400,240);
-
-ctx.fillStyle="#128812";
-ctx.strokeStyle="#ffee00";
-ctx.strokeRect(60,60,200,120);
-ctx.fillRect(60,60,200,120);
+setInterval(drawimg,100);

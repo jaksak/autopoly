c1eee608be59b536e0d67d3bb7c96f6fe4e853ee
jaksak <jkrzemien28@gmail.com>
2020-09-26 00:19:53 +0200

    short jav names

--- src/main/java/pl/longhorn/autopoly/street/StreetNameService.java
+++ src/main/java/pl/longhorn/autopoly/street/StreetNameService.java
@@ -3,34 +3,16 @@ package pl.longhorn.autopoly.street;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Random;
-import java.util.Scanner;
 
 @Service
 public class StreetNameService {
 
-    public static String getRandomLineFromFile(File f) throws FileNotFoundException {
-        String result = null;
-        Random rand = new Random();
-        int n = 0;
-        for (Scanner sc = new Scanner(f); sc.hasNext(); ) {
-            ++n;
-            String line = sc.nextLine();
-            if (rand.nextInt(n) == 0)
-                result = line;
-        }
-
-        return result;
-    }
+    private final StreetNames streetNames = new StreetNames();
 
     @SneakyThrows
     public String getRandom() {
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        var streetUrl = classloader.getResource("/streets.txt");
-        assert streetUrl != null;
-        var streetFile = new File(streetUrl.getFile());
-        return getRandomLineFromFile(streetFile);
+        Random random = new Random();
+        return streetNames.getNames()[random.nextInt(streetNames.getNames().length)];
     }
 }

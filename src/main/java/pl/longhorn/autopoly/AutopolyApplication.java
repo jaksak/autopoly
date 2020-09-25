92919a0ef6a123efc763609aa6de2e1e0e306e43
jaksak <jkrzemien28@gmail.com>
2020-09-25 20:03:21 +0200

    Initial commit

---
+++ src/main/java/pl/longhorn/autopoly/AutopolyApplication.java
@@ -0,0 +1,13 @@
+package pl.longhorn.autopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/longhorn/autopoly/AutopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.longhorn.autopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3c9e7cca4a839b4da4e74ca01c05b70a7d2b6d90
jaksak <jkrzemien28@gmail.com>
2020-09-28 16:34:52 +0200

    add dice combo repository

---
+++ src/main/java/pl/longhorn/autopoly/dice/DiceCombo.java
@@ -0,0 +1,7 @@
+package pl.longhorn.autopoly.dice;
+
+public class DiceCombo {
+    private String boardId;
+    private String playerId;
+    private int comboAmount;
+}
---
+++ src/main/java/pl/longhorn/autopoly/dice/DiceComboRepository.java
@@ -0,0 +1,22 @@
+package pl.longhorn.autopoly.dice;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DiceComboRepository {
+
+    private final Object monitor = new Object();
+    private DiceCombo diceCombo;
+
+    public DiceCombo get() {
+        synchronized (monitor) {
+            return diceCombo;
+        }
+    }
+
+    public void save(DiceCombo combo) {
+        synchronized (monitor) {
+            diceCombo = combo;
+        }
+    }
+}

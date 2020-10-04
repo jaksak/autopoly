e81daabc588b460ad9fc0e0234b7819a812235ed
jaksak <jkrzemien28@gmail.com>
2020-10-04 21:25:15 +0200

    house price as buying price function fix house lvl as house price bug

--- src/main/java/pl/longhorn/autopoly/district/field/definition/street/SequenceStreetFieldCommand.java
+++ src/main/java/pl/longhorn/autopoly/district/field/definition/street/SequenceStreetFieldCommand.java
@@ -10,26 +10,12 @@ public class SequenceStreetFieldCommand {
 
     private static final int INITIAL_PRICE = 50;
     private static final int PRICE_FLUX = 25;
-    private static final int HOUSE_PRICE_FLUX = 10;
     private final Randomizer randomizer;
-    private String latestDistrictId = "";
     private int latestPrice = INITIAL_PRICE;
-    private int latestHousePrice = 25;
 
     public StreetField generate(String id, String name, String districtId) {
         int currentPrice = latestPrice + randomizer.nextInt(PRICE_FLUX);
         latestPrice = currentPrice;
-        int housePrice = calculateHousePrice(districtId);
-        latestHousePrice = housePrice;
-        latestDistrictId = districtId;
-        return new StreetField(id, name, districtId, currentPrice, currentPrice / 50 * 10 + 25, housePrice, false);
-    }
-
-    private int calculateHousePrice(String districtId) {
-        if (latestDistrictId.equals(districtId)) {
-            return latestHousePrice;
-        } else {
-            return latestHousePrice + HOUSE_PRICE_FLUX;
-        }
+        return new StreetField(id, name, districtId, currentPrice, currentPrice / 50 * 10 + 25, 0, false);
     }
 }

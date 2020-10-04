ca03cd3ebedff4ce5b2d87fbab49df4557df37e0
jaksak <jkrzemien28@gmail.com>
2020-10-04 21:31:16 +0200

    flux price depends on district

--- src/main/java/pl/longhorn/autopoly/district/field/definition/street/SequenceStreetFieldCommand.java
+++ src/main/java/pl/longhorn/autopoly/district/field/definition/street/SequenceStreetFieldCommand.java
@@ -9,13 +9,20 @@ import pl.longhorn.autopoly.util.randomizer.Randomizer;
 public class SequenceStreetFieldCommand {
 
     private static final int INITIAL_PRICE = 50;
-    private static final int PRICE_FLUX = 25;
+    private static final int PRICE_FLUX_FOR_THE_SAME_DISTRICT = 40;
+    private static final int PRICE_FLUX = 10;
     private final Randomizer randomizer;
     private int latestPrice = INITIAL_PRICE;
+    private String latestDistrict;
 
     public StreetField generate(String id, String name, String districtId) {
-        int currentPrice = latestPrice + randomizer.nextInt(PRICE_FLUX);
+        int currentPrice = getCurrentPrice(districtId);
         latestPrice = currentPrice;
+        latestDistrict = districtId;
         return new StreetField(id, name, districtId, currentPrice, currentPrice / 50 * 10 + 25, 0, false);
+    }
+
+    private int getCurrentPrice(String currentDistrictId) {
+        return latestPrice + randomizer.nextInt(currentDistrictId.equals(latestDistrict) ? PRICE_FLUX_FOR_THE_SAME_DISTRICT : PRICE_FLUX);
     }
 }

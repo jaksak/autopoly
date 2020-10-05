ac8d231d52cfa4501e05a0b081458994d9c4d40c
jaksak <jkrzemien28@gmail.com>
2020-10-05 16:37:25 +0200

    use base path in requests

--- src/main/resources/static/requests.js
+++ src/main/resources/static/requests.js
@@ -1,41 +1,43 @@
-var gBStateButton= document.getElementById('getBoardStateButton');
-var gBConfigButton= document.getElementById('getBoardConfigButton');
+var gBStateButton = document.getElementById('getBoardStateButton');
+var gBConfigButton = document.getElementById('getBoardConfigButton');
 
-var boardID="string";
+var boardID = "string";
 var parcelsData;
 
+const BASE_API_PATH = 'https://autopoly.herokuapp.com:443';
+
 /* GET BOARD INIT STATE API REQUEST -beginning-*/
-window.onload=function(){
-	var requestURL = 'https://autopoly.herokuapp.com:443/board/config';
-	var doAfterResponse=function(a){
-		boardID=a.boardId;
-		parcelsData=a.fields;
-		namesSplited=false;
+window.onload = function () {
+	var requestURL = BASE_API_PATH + '/board/config';
+	var doAfterResponse = function (a) {
+		boardID = a.boardId;
+		parcelsData = a.fields;
+		namesSplited = false;
 		playersUpdate(a.players);
 	};
-	universalRequestFunction(requestURL,doAfterResponse);
+	universalRequestFunction(requestURL, doAfterResponse);
 }
-gBConfigButton.onclick=function(){
-	var requestURL = 'https://autopoly.herokuapp.com:443/board/config';
-	var doAfterResponse=function(a){
-		boardID=a.boardId;
-		parcelsData=a.fields;
-		namesSplited=false;
+gBConfigButton.onclick=function() {
+	var requestURL = BASE_API_PATH + '/board/config';
+	var doAfterResponse = function (a) {
+		boardID = a.boardId;
+		parcelsData = a.fields;
+		namesSplited = false;
 		playersUpdate(a.players);
 		alert((JSON.stringify(a)));
 	};
-	universalRequestFunction(requestURL,doAfterResponse);
+	universalRequestFunction(requestURL, doAfterResponse);
 }
 /* GET BOARD INIT STATE API REQUEST -end-*/
 
 /* GET CURRENT BOARD STATE API REQUEST -beginning-*/
-gBStateButton.onclick=function(){
-	var requestURL = 'https://autopoly.herokuapp.com:443/board/state?boardId='+boardID;
-	var doAfterResponse=function(a){
+gBStateButton.onclick=function() {
+	var requestURL = BASE_API_PATH + '/board/state?boardId=' + boardID;
+	var doAfterResponse = function (a) {
 		playersUpdate(a.players);
 		alert((JSON.stringify(a)));
 	};
-	universalRequestFunction(requestURL,doAfterResponse);
+	universalRequestFunction(requestURL, doAfterResponse);
 }
 /* GET CURRENT BOARD STATE API REQUEST -end-*/
 

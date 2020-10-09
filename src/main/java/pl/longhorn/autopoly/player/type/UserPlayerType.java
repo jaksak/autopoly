29544b42053e22af6677bfe39cdd1452adccb808
jaksak <jkrzemien28@gmail.com>
2020-10-09 18:09:52 +0200

    remove UserPlayerType

--- src/main/java/pl/longhorn/autopoly/player/type/UserPlayerType.java
+++
@@ -1,9 +0,0 @@
-package pl.longhorn.autopoly.player.type;
-
-public class UserPlayerType implements PlayerType {
-    @Override
-    // TODO: add time interval to auto action
-    public boolean shouldUseAutoAction() {
-        return false;
-    }
-}
